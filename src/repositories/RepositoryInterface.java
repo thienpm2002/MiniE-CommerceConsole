f7fb4cadceaf21f45e4829f1b70585323e48e26d
Thien <ledinhthienpm@gmail.com>
2026-03-12 17:02:19 +0700

    refactor: add param to findAll()

--- src/repositories/RepositoryInterface.java
+++ src/repositories/RepositoryInterface.java
@@ -7,7 +7,7 @@ public interface RepositoryInterface<L, T> {
 
     public T findById(L id);
 
-    public List<T> findAll();
+    public List<T> findAll(String condition);
 
     public void deleteById(L id);
 }
